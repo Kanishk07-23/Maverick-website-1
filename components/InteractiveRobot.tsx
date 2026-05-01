28b058df49980eeb99e2fc0bdbfe5f8a6e79b7ad
N0VAPRIME <kanishksonigra@gmail.com>
2026-05-01 23:28:49 +0530

    Fix build error: add missing Suspense import and apply AuroraBackground site-wide

--- components/InteractiveRobot.tsx
+++ components/InteractiveRobot.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useRef, useState, useMemo } from 'react';
+import { useRef, useState, useMemo, Suspense } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { 
   Float, 
